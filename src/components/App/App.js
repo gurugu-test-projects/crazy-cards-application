70b898d1a01d9db456ff8cc39ee1f91675255fcc
GuRuGu <peter.k.nets@gmail.com>
2021-09-24 18:52:35 +0300

    Set up mock database

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import useAsyncEffect from "use-async-effect";
+import axios from "axios";
 import './App.css';
 
 export const App = () => {
+  useAsyncEffect(async (isMounted) => {
+    const getData = async () => {
+      return await axios.get("http://localhost:3001/posts");
+    };
+
+    try {
+      const res = await getData();
+
+      if (!isMounted) {
+        return;
+      }
+
+      console.log(res.data);
+    } catch (err) {
+      console.error(err);
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      The App
     </div>
   );
 }
